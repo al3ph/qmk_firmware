209c9ee767f252ded8e5bb8a2b9bd571f3414e9c
aleph <aleph@al3ph.org>
2019-05-28 08:36:17 +0100

    more tweaks

--- keyboards/bbomb/bbomb.c
+++ keyboards/bbomb/bbomb.c
@@ -14,8 +14,28 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "bbomb.h"
-#include "rgblight.h"
+// #include "rgblight.h"
+#include "quantum/rgb_matrix.h"
 #include <print.h>
+
+led_config_t g_led_config = {
+	{
+  // Key Matrix to LED Index
+  		{ 0, 1, 2, 3, 4, 5, 6, 7, 8 },
+  	},
+    {
+	  // LED Index to Physical Position
+  		{ 0,  0 }, { 122,  0 }, { 244,  0 },
+  		{ 0,  32 }, { 122,  32 }, { 244,  32 },
+  		{ 0,  64 }, { 122,  64 }, { 244,  64 }
+	},
+	{
+		// LED Index to Flag
+		LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+		LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+		LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT
+	} 	
+};
 
 void matrix_init_kb(void) {
 	// put your keyboard start-up code here
