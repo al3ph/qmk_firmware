5d832c602359e263c5c81f4ad493292c82426142
Al Crate <aleph@al3ph.org>
2020-02-22 22:20:47 +0000

    fixes

--- keyboards/aleph/spirit/spirit.h
+++ keyboards/aleph/spirit/spirit.h
@@ -30,12 +30,12 @@
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, \
     K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, 	  \
-    K40, K41, K42,                K46,                K4A, K4B, K4C, K4D, \
+    K40, K41, K42,      K44,      K46,                K4A, K4B, K4C, K4D \
 ) { \
 /*          0      1      2      3      4      5      6      7      8      9      A      B      C      D   */  \
 /* 0 */ { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   K0D,   }, \
 /* 1 */ { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   KC_NO, }, \
 /* 2 */ { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   K2C,   K2D,   }, \
 /* 3 */ { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   KC_NO, }, \
-/* 4 */ { K40,   K41,   K42,   KC_NO, KC_NO, KC_NO, K46,   KC_NO, KC_NO, KC_NO, K4A,   K4B,   K4C,   K4D,   }  \
+/* 4 */ { K40,   K41,   K42,   KC_NO, K44,   KC_NO, K46,   KC_NO, KC_NO, KC_NO, K4A,   K4B,   K4C,   K4D,   }  \
 }
